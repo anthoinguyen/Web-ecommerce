962b5495175cd6f9a908efc9ae60731a90199dbb
leeminkan <16520950@gm.uit.edu.vn>
2019-12-24 15:14:26 +0700

    Update Search for item page

--- item.php
+++ item.php
@@ -1,3 +1,11 @@
+<?php
+if(isset($_POST['search'])){
+  $key = $_POST['key'];
+  if($key && $key != ""){
+  header("Location:search.php?key=${key}&pages=1");
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -49,11 +57,13 @@
           <a class="nav-link" href="#">Contact</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-      <input style="width:460px" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <form class="form-inline my-2 my-lg-0" method="POST" action="">
+        <input style="width:460px" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="key">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name = "search">Search</button>
         <button class="right-20" style="border: 1px solid green; margin-left:100px; padding:5px; border-radius: 5px;">
           <span class="fas fa-cart-plus" style="font-size:30px; color:green; opacity:0.7; margin-right:5px">
+
+            <!-- <span  style="color: red; position:absolute; top:-5px; left:20px; font-size:15px">1</span> -->
           </span>
           <span style="font-size:16px; position:relative; top:-5px; color:green; margin-right:10px">giỏ hàng</span>
           <span style=" position:relative; top:-4px; margin-right:10px; color:red">0</span>
